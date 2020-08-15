72a3f3f5f0f58d51a8485c222765299337b50684
Kuddleman <edl1155@gmail.com>
2020-08-15 11:53:46 -0700

    build displayResetButton function and refactor

--- index.js
+++ index.js
@@ -12,31 +12,50 @@ const message = document.getElementById('message')
 const rollBtn = document.getElementById('rollBtn')
 const resetBtn = document.getElementById('resetBtn')
 
+const displayResetButton = () => {
+  rollBtn.style.display = 'none'  
+      resetBtn.style.display = 'block'
+      resetBtn.style.margin = '30px auto'
+}
 
 
 const handleRollClick = () => {
  let randomNum = Math.ceil(Math.random() * 6)
  
   if ( player1Turn ) {
-    player1Score += randomNum
-    player1Scoreboard.textContent = player1Score
+    
     player2Dice.classList.remove('active')
     player1Dice.classList.add('active')
     player1Dice.textContent = randomNum
+    player1Score += randomNum
+    player1Scoreboard.textContent = player1Score
+
+    if ( player1Score >= 20 ) {
+      message.textContent = 'Player 1 WON!'
+      displayResetButton()
+    } else {
     message.textContent = 'It\'s Player 2 Turn'
-    
+    }
 
   } else {
-    player2Score += randomNum
-    player2Scoreboard.textContent = player2Score
+    
     player1Dice.classList.remove('active')
     player2Dice.classList.add('active')
     player2Dice.textContent = randomNum
-    message.textContent = 'It\'s Player 1 Turn'
-  } 
+    player2Score += randomNum
+    player2Scoreboard.textContent = player2Score
 
+    if ( player2Score >= 20 ) {
+      message.textContent = 'Player 2 WON!'
+      displayResetButton()
+    } else {
+      message.textContent = 'It\'s Player 1 Turn'
+    } 
+
+   
+  }
   player1Turn = !player1Turn
 }
 
-
 rollBtn.addEventListener('click', handleRollClick)
+
