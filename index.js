92aade1664ed22ae718c838317be26e148758659
Kuddleman <edl1155@gmail.com>
2020-08-15 09:16:16 -0700

    remove unnecessary comments

--- index.js
+++ index.js
@@ -14,23 +14,18 @@ const rollBtn = document.getElementById('rollBtn')
 const resetBtn = document.getElementById('resetBtn')
 
 
-// add event listener to roll dice button.  Have it
-// log out a random number between 1 and 6 inclusive
+
 const handleRollClick = () => {
  let randomNum = Math.ceil(Math.random() * 6)
- console.log( randomNum )
- // 1. Find out which players turn it is
-     // 2. log out the value e.g. "Player 1 rolled 5"
-     // 3. Switch the turn back to the other player
-
+ 
   if ( player1Turn ) {
     console.log(`Player 1 rolled a ${ randomNum }`)
-    
   } else {
     console.log(`Player 2 rolled a ${ randomNum }`)
-    
   } 
   player1Turn = !player1Turn
 }
+
+
 
 rollBtn.addEventListener('click', handleRollClick)
