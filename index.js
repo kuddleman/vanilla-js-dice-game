7b7b1cf11e4f728ed4aaa25d6eb14ebd745d44fa
Kuddleman <edl1155@gmail.com>
2020-08-15 09:10:46 -0700

    determine which player's turn it is

--- index.js
+++ index.js
@@ -19,6 +19,18 @@ const resetBtn = document.getElementById('resetBtn')
 const handleRollClick = () => {
  let randomNum = Math.ceil(Math.random() * 6)
  console.log( randomNum )
+ // 1. Find out which players turn it is
+     // 2. log out the value e.g. "Player 1 rolled 5"
+     // 3. Switch the turn back to the other player
+
+  if ( player1Turn ) {
+    console.log(`Player 1 rolled a ${ randomNum }`)
+    
+  } else {
+    console.log(`Player 2 rolled a ${ randomNum }`)
+    
+  } 
+  player1Turn = !player1Turn
 }
 
 rollBtn.addEventListener('click', handleRollClick)
